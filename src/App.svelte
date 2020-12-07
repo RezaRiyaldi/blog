e5067bc1b6873f4d221a5b183664bf36f4cd7960
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2020-12-07 17:08:49 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</svelte:head>
+
+<script>
+	import {
+	  Collapse,
+	  Navbar,
+	  NavbarToggler,
+	  NavbarBrand,
+	  Nav,
+	  NavItem,
+	  NavLink,
+	  UncontrolledDropdown,
+	  DropdownToggle,
+	  DropdownMenu,
+	  DropdownItem
+	} from 'sveltestrap';
+  
+	let isOpen = false;
+  
+	function handleUpdate(event) {
+	  isOpen = event.detail.isOpen;
+	}
+  </script>
+  
+  <Navbar color="light" light expand="md">
+	<NavbarBrand href="/">sveltestrap</NavbarBrand>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+	  <Nav class="ml-auto" navbar>
+		<NavItem>
+		  <NavLink href="#components/">Components</NavLink>
+		</NavItem>
+		<NavItem>
+		  <NavLink href="https://github.com/RezaRiyaldi">GitHub</NavLink>
+		</NavItem>
+		<UncontrolledDropdown nav inNavbar>
+		  <DropdownToggle nav caret>Options</DropdownToggle>
+		  <DropdownMenu right>
+			<DropdownItem>Option 1</DropdownItem>
+			<DropdownItem>Option 2</DropdownItem>
+			<DropdownItem divider />
+			<DropdownItem>Reset</DropdownItem>
+		  </DropdownMenu>
+		</UncontrolledDropdown>
+	  </Nav>
+	</Collapse>
+  </Navbar>
